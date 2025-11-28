df40c986a4a713373d5f04ebd2ee1dae838f26b8
Joshua Luke Parris <joshua.parris@cornerstone.edu.au>
2025-11-28 11:12:51 +1100

    fix(room): exportSchema=false + room.schemaLocation for schema export

--- app/src/main/java/com/parris/joshtap/data/AppDatabase.kt
+++ app/src/main/java/com/parris/joshtap/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [TrackEntity::class, CardEntity::class, CardTrackJoin::class], version = 2)
+@Database(entities = [TrackEntity::class, CardEntity::class, CardTrackJoin::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun appDao(): AppDao
 
